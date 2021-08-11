d43cca690a73b1489847e6efb3166fd0680b4caa
mathilde sempere mathilde-sempere@hotmail.fr <mathilde-sempere@hotmail.fr>
2021-08-11 16:45:06 +0200

    rails yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create({
+                    name: 'Sushi shop',
+                    address: '33 rue du loup FRANCE ',
+                    phone_number: '04 68 52 28 27',
+                    category: 'japanese'
+                  })
+
+Restaurant.create({
+                    name: 'Brasserie chez Jean',
+                    address: '103 allee des quinconces',
+                    phone_number: '04 68 52 28 27',
+                    category: 'french'
+                  })
+
+Restaurant.create({
+                    name: 'La saucisse du belge',
+                    address: '18 rue des belges FRANCE',
+                    phone_number: '04 68 52 28 27',
+                    category: 'belgian'
+                  })
+
+Restaurant.create({
+                    name: 'Robert le pizzaiolo',
+                    address: '45 Rue des ravioles FRANCE',
+                    phone_number: '04 68 52 28 27',
+                    category: 'italian'
+                  })
+
+Restaurant.create({
+                    name: 'Les nems de paris',
+                    address: '123 rue des remparts FRANCE',
+                    phone_number: '04 68 52 28 27',
+                    category: 'chinese'
+                  })
